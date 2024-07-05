02ecf60c5278e3d6df5ef2f2fe52eae1b323acf5
Yvonne Cheng <cheng.yu.yvonne@gmail.com>
2024-07-05 17:02:41 +0000

    fixing errors for automation

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,7 @@
 import unittest, sys
 
 sys.path.append('../') # imports python file from parent directory
+sys.path.append('../')
 from app import app #imports flask app object
 
 class BasicTests(unittest.TestCase):
